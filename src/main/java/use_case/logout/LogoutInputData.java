1b4679ed251e9d0d5eb4fcfaefccc497aa478093
Vab Gupta <vabgupta89@gmail.com>
2024-10-21 12:43:45 -0400

    Checkstyle error was corrected.

--- src/main/java/use_case/logout/LogoutInputData.java
+++ src/main/java/use_case/logout/LogoutInputData.java
@@ -6,7 +6,6 @@ package use_case.logout;
 public class LogoutInputData {
     private final String username;
 
-
     public LogoutInputData(String username) {
         this.username = username;
     }
